27b2a94a3f32f735307ed666a9d10e3200ab09fb
hsfalaska <sedjroalladassi@outlook.com>
2017-08-06 07:44:50 +0100

    Setup and static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="text-center">
+    <p>&copy; {{date('Y')}} &middot; Laracarte by <a href="https://twitter.com/etsmo">@etsmo</a> - <a href="https://laramap.com">Laramap</a> clone app</p>
+    <p><strong>This app has been build  for learning purposes.</strong></p>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1 class="text-center">Map hey !!!</h1>
+@stop
